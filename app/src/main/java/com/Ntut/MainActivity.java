afafb73319f3f3a38e6625e559f38d75d6bc8e30
Black Maple <yaoandy107@gmail.com>
2017-04-20 21:55:15 +0800

    Initial

--- app/src/main/java/com/Ntut/MainActivity.java
+++ app/src/main/java/com/Ntut/MainActivity.java
@@ -1,14 +1,83 @@
 package com.Ntut;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener {
+
+    private BottomNavigationBar bottomNavigationBar;
+    private int lastSelectedPosition;
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        fggf/
+        setupNavigation();
+
+    }
+
+    private void setupNavigation(){
+        bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar.setTabSelectedListener(this);
+        bottomNavigationBar
+                .setMode(BottomNavigationBar.MODE_FIXED);
+        bottomNavigationBar.
+                setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_RIPPLE);
+        bottomNavigationBar
+                .setActiveColor(R.color.colorPrimary)
+                .setInActiveColor("#FFFFFF")
+                .setBarBackgroundColor("#ECECEC");
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.course_icon, "課程"))
+                .addItem(new BottomNavigationItem(R.drawable.calendar_icon, "行事曆"))
+                .addItem(new BottomNavigationItem(R.drawable.credit_icon, "學分"))
+                .addItem(new BottomNavigationItem(R.drawable.nportal_icon, "入口網"))
+                .addItem(new BottomNavigationItem(R.drawable.account_setting_icon, "設定"))
+                .initialise();
+    }
+
+
+    @Override
+    public void onTabSelected(int position) {
+        lastSelectedPosition = position;
+        switchFragment(position);
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+    }
+
+    private void switchFragment(int position) {
+        switch (position) {
+            case 0:
+                break;
+        }
+    }
+
+    private void changeFragment(Fragment newFragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if (currentFragment != null) {
+            if (currentFragment.equals(newFragment)) {
+                return;
+            }
+            if (!newFragment.isAdded()) {
+                transaction.hide(currentFragment).add(R.id.fragment_container, newFragment).commit();
+            } else {
+                transaction.hide(currentFragment).show(newFragment).commit();
+            }
+        } else {
+            transaction.add(R.id.fragment_container, newFragment).commit();
+        }
+        currentFragment = newFragment;
     }
 }
