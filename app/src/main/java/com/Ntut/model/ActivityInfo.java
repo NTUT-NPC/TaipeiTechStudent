5d78c8dcbf1be9ba1d1ce825faea0d90f79cfce9
Black Maple <yaoandy107@gmail.com>
2017-08-04 03:23:03 +0800

    Code cleanup

--- app/src/main/java/com/Ntut/model/ActivityInfo.java
+++ app/src/main/java/com/Ntut/model/ActivityInfo.java
@@ -80,10 +80,6 @@ public class ActivityInfo {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
     public String getLocation() {
         return location;
     }
